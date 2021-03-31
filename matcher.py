8038efcaed7650d9768590e775dba2b7f85d290b
Daniil Timoschenko <carnolio@gmail.com>
2021-04-01 01:19:11 +0500

    Add files via upload

--- matcher.py
+++ matcher.py
@@ -14,13 +14,7 @@
 #Пару из символа и индекса в строке — скобка, для которой не удалось найти закрывающую.
 #40 91 123 60 ([{<
 #41 93 125 62 )]}>
-def isMatch(strMatch, *brackets):
-    isCorrect = True
-    for ch in strMatch:
-       print(strMatch)
-    print(*brackets)
-
-def formBrackets(closeList):
+def formBrackets(openList):
     closeList = []
     for ch in openList:
         if ch == "(":
@@ -35,17 +29,55 @@ def formBrackets(closeList):
     #print(closeList)
     return closeList
 
-parseList = []
+def isMatch(strMatch, openBr, closeBr):
+    isCorrect = False
+    firstIsOpen = False
+    print(strMatch)
+    stack = []
+    positions = []
+    depth = 0
 
+    #отбросили лишнее оставили только скобки и запомнили номера в строке
+    for i in range(len(strMatch)):
+        if (strMatch[i] in openBr) or (strMatch[i] in closeBr):
+           stack.append(strMatch[i])
+           positions.append(i)
+    #промежуточные итоги
+    print(stack)
+    print(positions)
+
+    n=0
+    lenStack = len(stack)
+    while stack !="" or n < lenStack:
+        for i in range(lenStack):
+            for j in range(len(openBr)):
+                if (i+1) <= len(stack) and stack[i] in openBr and stack[i+1] in closeBr:
+                    if (stack[i]==openBr[j]) and (stack[i+1]==closeBr[j]):
+                        stack.pop(i)
+                        stack.pop(i)
+                        positions.pop(i)
+                        positions.pop(i)
+                        n+=2
+                        print(stack)
+                        print(positions)
+        print(i)
+    return isCorrect
+
+
+parseList = []
 #stringForMatch = input("Введите строку для проверки: ")
-brackets = input("Введите набор открывающихся скобок ([{<: ")
+#stringForMatch = "(a+[b*c]-{d/3})"
+stringForMatch = "(a+[b*c)-17]"
+#brackets = input("Введите набор открывающихся скобок ([{<: ")
+brackets = "(["
 openList = list(brackets)
 
 #
 #for i in range(len(bracketsList)):
 #    print(ord(bracketsList[i]))
     #print(bracketsList[i])
-print(openList)
-closeList = formBrackets(openList)
-print(closeList)
 
+closeList = formBrackets(openList)
+print(openList)
+print(closeList)
+isMatch(stringForMatch,openList,closeList)
