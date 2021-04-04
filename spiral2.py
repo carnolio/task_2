c4330f7e41c0b8885c4cff763abff30834e05815
Daniil Timoschenko <carnolio@gmail.com>
2021-04-04 11:11:51 +0500

    Add files via upload

--- spiral2.py
+++ spiral2.py
@@ -1,32 +1,31 @@
 def getSpiral(n):
     dx, dy = 1, 0
     x, y = 0, 0
-    #Заполняем массив значениями нужного размера
-    spiralArray = [[None] * n for j in range(n)]
-
+    myarray = [[None] * n for j in range(n)]
     for i in range(1, n ** 2 + 1):
         myarray[x][y] = i
         nx = x + dx
         ny = y + dy
-        if 0 <= nx < n and 0 <= ny < n and spiralArray[nx][ny] == None:
+        if 0 <= nx < n and 0 <= ny < n and myarray[nx][ny] == None:
             x = nx
             y = ny
         else:
-            dx = -dy
-            dy = dx
-            x = x + dx
-            y = y + dy
-    return spiralArray
+            dx, dy = -dy, dx
+            x, y = x + dx, y + dy
+
+    return myarray
 
 
-def printSpiral(spiralArray):
-    n = range(len(spiralArray))
+def printSpiral(myarray):
+    n = range(len(myarray))
     for y in n:
         for x in n:
-            print(spiralArray[x][y], end=' ')
+            print(myarray[x][y], end=' ')
         print()
 
 try:
+    print("Введите целое положительное число:")
     n = int(input())
-except Exception:
     printSpiral(getSpiral(n))
+except Exception:
+    print("Вероятно ввели не число")
